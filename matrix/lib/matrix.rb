9cec37f96b562d0687da6c666065ae507993b483
omar paro <omar.parto@gmail.com>
2015-09-06 12:06:28 +0100

    Implement the command line tool to build and interact with the bitmap

--- matrix/lib/matrix.rb
+++ matrix/lib/matrix.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 class Matrix
   attr_accessor :matrix, :default_value, :rows, :columns
 
@@ -19,7 +21,7 @@ class Matrix
   # if value is not passed to the method use @default_value
   def reset(value = nil) 
     value ||= @default_value
-    raise_wrong_pixel_value(value)
+    raise_is_not_a_string(value)
     @matrix.clear
     index = 0
 
@@ -33,7 +35,7 @@ class Matrix
   def set_color_to_pixel(row, column, colour)
     raise_wrong_row(row)
     raise_wrong_column(column)    
-    raise_wrong_pixel_value(colour)
+    raise_is_not_a_string(colour)
     pixel = ((row - 1) * @columns) + (column - 1)
     @matrix[pixel] = colour
   end 
@@ -45,7 +47,7 @@ class Matrix
     last_row  = start_row > end_row ? start_row : end_row
     raise_wrong_row(first_row, last_row)
     raise_wrong_column(column)    
-    raise_wrong_pixel_value(colour)
+    raise_is_not_a_string(colour)
        
     if start_row == end_row
       set_color_to_pixel(start_row, column, colour)
@@ -66,12 +68,12 @@ class Matrix
 
   # drow a orizzantal line from a pixel to another pixel
   # with a give colour 
-  def draw_orizzontal_line(start_column, end_column, row, colour)
+  def draw_horizontal_line(start_column, end_column, row, colour)
     first_column = start_column > end_column ? end_column : start_column
     last_column  = start_column > end_column ? start_column : end_column
     raise_wrong_row(row)
     raise_wrong_column(start_column, end_column)    
-    raise_wrong_pixel_value(colour)
+    raise_is_not_a_string(colour)
 
     if start_column == end_column 
       set_color_to_pixel(start_column, row, colour)
@@ -90,7 +92,7 @@ class Matrix
   def paint_area(row, column, colour)
     raise_wrong_row(row)
     raise_wrong_column(column)    
-    raise_wrong_pixel_value(colour)
+    raise_is_not_a_string(colour)
 
     @visited = []
     pixel = pixel(row, column - 1)
@@ -103,7 +105,8 @@ class Matrix
     @cell_value = @matrix[cell] if @cell_value.nil?
     value = @matrix[cell]
 
-    if !cell.nil? && !@visited.include?(cell) && value == @cell_value && cell >= 0 and cell < @dimension
+    if !cell.nil? && !@visited.include?(cell) && 
+       value == @cell_value && cell >= 0 and cell < @dimension
       @visited << cell
       @matrix[cell] = colour 
       # TODO: refactor the following if statements
@@ -164,29 +167,149 @@ class Matrix
     end
   end
 
-  def init
-    # TODO: add console input 	   
-  end
-
   private def pixel(row, column)
     ((row - 1) * @columns) + column
   end
 
   private def raise_wrong_row(*rows) 
     if rows.any?{|value| value > @rows}
-      raise "The row selected is greater than #{@rows}"
+      capture_exception("The row selected is greater than #{@rows}")
     end  
   end
 
   private def raise_wrong_column(*columns) 
     if columns.any?{|value| value > @columns}
-      raise "The column selected is greater than #{@columns}"
+      capture_exception("The column selected is greater than #{@columns}")
     end  
   end
   
-  private def raise_wrong_pixel_value(value)
+  private def raise_is_not_a_string(value)
     if value.nil? || !value.is_a?(String)
-      raise "The value is nil or is not a string"
+      capture_exception("The value is nil or is not a string") 
+    end
+  end
+
+  # utility method to capture exceptions
+  private def capture_exception(exception)
+    begin
+      raise exception
+    rescue => detail
+      puts detail
+    end	      
+  end
+
+  def self.exctract_arguments(values)
+    values.delete_at(0)
+    values.map do |value|
+      if self.is_number? value
+        value.to_i
+      else
+	value
+      end
+    end
+  end
+
+  def self.is_number?(string_value)
+    true if Integer(string_value) rescue false
+  end
+
+  # create a regex expression from a series fo commands
+  def self.create_regex(command, number_of_digits = 0)
+    if !command.is_a?(String)
+      capture_exception("The command argument must be a string") 
+    end
+    if !number_of_digits.is_a?(Integer)
+      capture_exception("The number_of_digits argument must be a string") 
+    end
+
+    if number_of_digits != 0
+      arguments = [command, 
+		   *(0...number_of_digits).map{|x| "d+"}, "w"
+                  ].join("\\s+\\")
+    else
+      arguments = command
+    end
+
+    return Regexp.new(arguments)
+  end
+
+  def self.init
+    is_matrix_created = false
+    create_bitmap_usage = <<-eos
+      Usage: I [number] [number] -- program that simulates a basic interactive 
+                                    bitmap editor
+
+        where:
+            number(first)   rapresent the number of rows in the bitmap
+            number(second)  rapresent the number of column in the bitmap
+    
+    eos
+
+    interective_bitmap_usage = <<-eos
+       Usage : 
+           C           - Clears the table, setting all pixels to white (O).
+           L X Y C     - Colours the pixel (X,Y) with colour C.
+           V X Y1 Y2 C - Draw a vertical segment of colour C in column X betwee
+                         rows Y1 and Y2 (inclusive).
+           H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between
+                         columns X1 and X2 (inclusive).
+           F X Y C     - Fill the region R with the colour C. R is defined as: 
+                         Pixel (X,Y) belongs to R. Any other pixel which is the
+                         same colour as (X,Y) and shares a common side with any
+                         pixel in R also belongs to this region.
+           S           - Show the contents of the current image
+           X           - Terminate the session
+    eos
+        
+    puts create_bitmap_usage
+
+    $stdin.each_line do |line|	
+       input = String(line.chomp)
+       values = String(line.chomp).split(/\s+/)
+       command = values[0]
+       if command == "X"
+         return
+       end	       
+        
+       if !is_matrix_created
+         rows = values[1].to_i
+         columns = values[2].to_i
+         if values.count == 3 && command == "I" && 
+            self.is_number?(rows) && 
+	    self.is_number?(columns) 
+           
+           if rows != 0 && columns != 0 
+             @new_matrix = Matrix.new(rows, columns)
+	     is_matrix_created = true
+	     puts "Bitmap #{rows} X #{columns} has been created\n"
+             puts interective_bitmap_usage
+	   else 	 
+	     puts create_bitmap_usage	   
+	   end	   
+         else 	 
+	   puts create_bitmap_usage	   
+         end	   
+       else
+	 arguments = exctract_arguments(values)
+         case input
+           when create_regex("C")
+             @new_matrix.reset
+           when create_regex("L", 2)
+             @new_matrix.set_color_to_pixel(*arguments)
+           when create_regex("V", 3)
+             @new_matrix.draw_vertical_line(*arguments)
+           when create_regex("H", 3)
+             @new_matrix.draw_horizontal_line(*arguments)
+           when create_regex("F", 2)
+             @new_matrix.paint_area(*arguments)
+           when create_regex("S")
+             @new_matrix.to_s    
+          else  
+             puts "The command #{values.join(" ")} is not valid please " + 
+                  "select the following options"
+	     puts interective_bitmap_usage   
+         end
+      end 
     end
   end
 
@@ -204,4 +327,6 @@ class Matrix
       end
     end 
   end
-end
+  self
+end.init
+
